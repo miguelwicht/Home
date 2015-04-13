1034667d3073ba003f6bfbac518e9ad9ad5ed85c
Miguel dos Santos Vaz Dias Wicht <mail@miguelwicht.com>
2015-04-13 13:39:07 +0200

    Initial Commit

---
+++ OpenhabApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenhabApi
+//
+//  Created by Miguel dos Santos Vaz Dias Wicht on 13/04/15.
+//  Copyright (c) 2015 Miguel dos Santos Vaz Dias Wicht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
